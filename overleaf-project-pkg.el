3fa74b6ca02e21b2a0bc044c5eeac0615a13fdf6
Jamie Cui <jamie.cui@outlook.com>
2026-06-05 00:24:37 +0800

    chore: add package metadata descriptor

---
+++ overleaf-project-pkg.el
@@ -0,0 +1,13 @@
+;;; overleaf-project-pkg.el --- Package metadata for overleaf-project  -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(define-package "overleaf-project" "2.0.0"
+  "Clone, push, and pull full Overleaf projects with Git"
+  '((emacs "29.4")
+    (websocket "1.15")
+    (webdriver "0.1"))
+  :url "https://github.com/Jamie-Cui/overleaf-project"
+  :keywords '("hypermedia" "tex" "tools")
+  :authors '(("Jamie Cui" . "jamie.cui@outlook.com"))
+  :maintainers '(("Jamie Cui" . "jamie.cui@outlook.com")))
+
+;;; overleaf-project-pkg.el ends here
